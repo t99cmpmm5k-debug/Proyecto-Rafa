8d48c3559ded2a3f709e85be12ea1ece3a0ff0db
t99cmpmm5k-debug <t99cmpmm5k@privaterelay.appleid.com>
2026-07-18 19:39:37 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="proyecto-rafa-v14-1";
+const CACHE="proyecto-rafa-v15";
 self.addEventListener("install",e=>{
   self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html"])));
