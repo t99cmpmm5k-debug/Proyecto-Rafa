46d9f8afbe3eb4c4216d525c22bf1e91f3bed550
t99cmpmm5k-debug <t99cmpmm5k@privaterelay.appleid.com>
2026-07-18 14:19:39 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,22 @@
-
-const CACHE="proyecto-rafa-v1";
-const ASSETS=["./","index.html","styles.css","app.js","manifest.json","icon.svg"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE="proyecto-rafa-v4";
+self.addEventListener("install",event=>{
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(["./","./index.html"])));
+});
+self.addEventListener("activate",event=>{
+  event.waitUntil(Promise.all([
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),
+    self.clients.claim()
+  ]));
+});
+self.addEventListener("fetch",event=>{
+  if(event.request.mode==="navigate"){
+    event.respondWith(fetch(event.request).then(response=>{
+      const copy=response.clone();
+      caches.open(CACHE).then(cache=>cache.put("./index.html",copy));
+      return response;
+    }).catch(()=>caches.match("./index.html")));
+    return;
+  }
+  event.respondWith(fetch(event.request).catch(()=>caches.match(event.request)));
+});
