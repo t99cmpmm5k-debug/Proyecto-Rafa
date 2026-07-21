727b68bf703f51a7a08568d030dc621dcee04472
t99cmpmm5k-debug <t99cmpmm5k@privaterelay.appleid.com>
2026-07-22 00:41:47 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="corredor-solido-v22-5-importador-universal";
+const CACHE="corredor-solido-v22-6-importador-universal-corregido";
 self.addEventListener("install",e=>{
   self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./hero-boceto-c-azul.jpg"])));
