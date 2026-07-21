b6bd5813ed9adce61b3bf82cf6f561db57d3c89e
t99cmpmm5k-debug <t99cmpmm5k@privaterelay.appleid.com>
2026-07-21 12:42:53 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,22 +1,4 @@
-const CACHE="proyecto-corredor-solido-v20.9-8-professional-charts";
-self.addEventListener("install",e=>{
-  self.skipWaiting();
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html"])));
-});
-self.addEventListener("activate",e=>{
-  e.waitUntil(Promise.all([
-    caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))),
-    self.clients.claim()
-  ]));
-});
-self.addEventListener("fetch",e=>{
-  if(e.request.mode==="navigate"){
-    e.respondWith(fetch(e.request).then(r=>{
-      const c=r.clone();
-      caches.open(CACHE).then(x=>x.put("./index.html",c));
-      return r;
-    }).catch(()=>caches.match("./index.html")));
-    return;
-  }
-  e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
-});
+const CACHE="corredor-solido-v20-9-1-inicio-d";
+self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html"]))) });
+self.addEventListener("activate",e=>{e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()]))});
+self.addEventListener("fetch",e=>{if(e.request.mode==="navigate"){e.respondWith(fetch(e.request,{cache:"no-store"}).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put("./index.html",copy));return r}).catch(()=>caches.match("./index.html")));return;}e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))});
